23589104e64ff9fc74a75ee42cfc7a218c0c6954
SINtyanneru <irjitgd@gmail.com>
2023-04-04 17:28:07 +0900

    0

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # RUMISESSION
+___
+## What is RUMISESSION
+RUMISESSION" is a session library created in PHP.<BR>
+It was developed exclusively for "RumiServer" and released as OSS.<BR>
+
+## るみせっしょんってな～に？
+「るみせっしょん」は、PHPで作成されたセッションライブラリです。<BR>
+るみさーばー専用に開発したものを、OSSにして公開したものです。<BR>
---
+++ RUMISESSION.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * るみセッション V1.0
+ */
+
+class RUMISESSION{
+	public $COOKIE_NAME = "";
+	public $SESSION_DATA = array();
+	public $SESSION_ID = "";
+
+	public function RSESSION_START(){
+
+	}
+
+	public function RSESSION_SET($NAME, $VALUE){
+
+	}
+
+	public function RSESSION_GET($NAME){
+
+	}
+
+	public function RSESSION_DEL($NAME){
+
+	}
+
+	public function RSESSION_RG(){
+
+	}
+}
+?>
